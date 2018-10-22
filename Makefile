5383015e3ecacfba3dd1b62d734e849914416e96
20180123 <wildrl@login01.cluster.cs.jmu.edu>
2018-10-22 19:18:45 -0400

    data files created to use for testing

--- Makefile
+++ Makefile
@@ -6,11 +6,11 @@ demo: solve_dpend dpend_mpfr
 	./solve_dpend 0.0 10.0 90.0 0.00 -10.0 0.0 1000 > outfile-original.txt
 	./dpend_mpfr  0.0 10.0 90.0 0.00 -10.0 0.0 1000 64 > outfile-mpfr.txt
 
-alldata: dpend_mpfr
-	./dpend_mpfr 0.0 10.0 90.0 0.00 -10.0 0.0 1000 8 > mpfr8.txt
-	./dpend_mpfr 0.0 10.0 90.0 0.00 -10.0 0.0 1000 16 > mpfr16.txt
-	./dpend_mpfr 0.0 10.0 90.0 0.00 -10.0 0.0 1000 32 > mpfr32.txt
-	./dpend_mpfr 0.0 10.0 90.0 0.00 -10.0 0.0 1000 64 > mpfr64.txt
+data: dpend_mpfr
+	./dpend_mpfr 0.0 10.0 90.0 0.00 -10.0 0.0 1000 8 > ./mpfr_data/mpfr8.txt
+	./dpend_mpfr 0.0 10.0 90.0 0.00 -10.0 0.0 1000 16 > ./mpfr_data/mpfr16.txt
+	./dpend_mpfr 0.0 10.0 90.0 0.00 -10.0 0.0 1000 32 > ./mpfr_data/mpfr32.txt
+	./dpend_mpfr 0.0 10.0 90.0 0.00 -10.0 0.0 1000 64 > ./mpfr_data/mpfr64.txt
 
 solve_dpend: solve_dpend.c
 	$(CC) -g -O3 -Wall -std=c99 -o $@ $< -lm
--- dpend_viz.py
+++ dpend_viz.py
@@ -9,10 +9,10 @@ import os
 NSTEPS=1000
 step=0
 
-mpfr8_file = open("mpfr8.txt", "r")
-mpfr16_file = open("mpfr16.txt", "r")
-mpfr32_file = open("mpfr32.txt", "r")
-mpfr64_file = open("mpfr64.txt", "r")
+mpfr8_file = open("./mpfr_data/mpfr8.txt", "r")
+mpfr16_file = open("./mpfr_data/mpfr16.txt", "r")
+mpfr32_file = open("./mpfr_data/mpfr32.txt", "r")
+mpfr64_file = open("./mpfr_data/mpfr64.txt", "r")
 
 t=[NSTEPS]
 x1=[NSTEPS]
@@ -29,18 +29,22 @@ for line in mpfr8_file.readlines():
   y2.append(float(step_data[4]))
   step+=1
 
-make_movie=True
-params = {'backend': 'ps',
-          'font.size': 20,
-          'font.family':'serif',
-          'font.serif':['Times'],
-          'ps.usedistiller': 'xpdf',
-          'text.usetex': True,
-          }
-plt.rcParams.update(params)
-plt.ion()
-fig = plt.figure(figsize=(9.6,5.4),dpi=100) # 1920x1080
-fig.subplots_adjust(left=0, right=1, top=1, bottom=0,hspace=0.02,wspace=0.02)
-ax = fig.add_subplot(111)
-ax.axis('off') # no frame
+plt.plot(4,7)
+
+
+
+#make_movie = TRUE
+#params = {'backend': 'ps',
+#          'font.size': 20,
+#          'font.family':'serif',
+#          'font.serif':['Times'],
+#          'ps.usedistiller': 'xpdf',
+#          'text.usetex': True,
+#          }
+#plt.rcParams.update(params)
+#plt.ion()
+#fig = plt.figure(figsize=(9.6,5.4),dpi=100) # 1920x1080
+#fig.subplots_adjust(left=0, right=1, top=1, bottom=0,hspace=0.02,wspace=0.02)
+#ax = fig.add_subplot(111)
+#ax.axis('off') # no frame
 

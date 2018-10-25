024543f0896278b8a7010dde11f28d575f6b89b3
Wild <wildrl@l24831.cs.jmu.edu>
2018-10-25 14:05:57 -0400

    Edit to python code to read in exact percision.

--- dpend_viz.py
+++ dpend_viz.py
@@ -8,6 +8,9 @@ from matplotlib.patches import Circle
 
 NSTEPS=0
 
+print np.finfo(np.longdouble)
+print np.finfo(np.float64)
+
 mpfr8_file = open("./mpfr_data/cartesian8.txt", "r")
 mpfr16_file = open("./mpfr_data/cartesian16.txt", "r")
 mpfr32_file = open("./mpfr_data/cartesian32.txt", "r")
@@ -22,44 +25,44 @@ y2_8, y2_16, y2_32, y2_64, y2_128 = ([] for i in range(5))
 
 for line in mpfr8_file.readlines():
   step_data_8 = line.split(' ', 5)
-  t_8.append(float(step_data_8[0]))
-  x1_8.append(float(step_data_8[1]))
-  y1_8.append(float(step_data_8[2]))
-  x2_8.append(float(step_data_8[3]))
-  y2_8.append(float(step_data_8[4]))
+  t_8.append(np.float16(step_data_8[0]))
+  x1_8.append(np.float16(step_data_8[1]))
+  y1_8.append(np.float16(step_data_8[2]))
+  x2_8.append(np.float16(step_data_8[3]))
+  y2_8.append(np.float16(step_data_8[4]))
   NSTEPS+=1
 
 for line in mpfr16_file.readlines():
   step_data_16 = line.split(' ', 5)
-  t_16.append(float(step_data_16[0]))
-  x1_16.append(float(step_data_16[1]))
-  y1_16.append(float(step_data_16[2]))
-  x2_16.append(float(step_data_16[3]))
-  y2_16.append(float(step_data_16[4]))
+  t_16.append(np.float16(step_data_16[0]))
+  x1_16.append(np.float16(step_data_16[1]))
+  y1_16.append(np.float16(step_data_16[2]))
+  x2_16.append(np.float16(step_data_16[3]))
+  y2_16.append(np.float16(step_data_16[4]))
 
 for line in mpfr32_file.readlines():
   step_data_32 = line.split(' ', 5)
-  t_32.append(float(step_data_32[0]))
-  x1_32.append(float(step_data_32[1]))
-  y1_32.append(float(step_data_32[2]))
-  x2_32.append(float(step_data_32[3]))
-  y2_32.append(float(step_data_32[4]))
+  t_32.append(np.float32(step_data_32[0]))
+  x1_32.append(np.float32(step_data_32[1]))
+  y1_32.append(np.float32(step_data_32[2]))
+  x2_32.append(np.float32(step_data_32[3]))
+  y2_32.append(np.float32(step_data_32[4]))
 
 for line in mpfr64_file.readlines():
   step_data_64 = line.split(' ', 5)
-  t_64.append(float(step_data_64[0]))
-  x1_64.append(float(step_data_64[1]))
-  y1_64.append(float(step_data_64[2]))
-  x2_64.append(float(step_data_64[3]))
-  y2_64.append(float(step_data_64[4]))
+  t_64.append(np.float64(step_data_64[0]))
+  x1_64.append(np.float64(step_data_64[1]))
+  y1_64.append(np.float64(step_data_64[2]))
+  x2_64.append(np.float64(step_data_64[3]))
+  y2_64.append(np.float64(step_data_64[4]))
 
 for line in mpfr128_file.readlines():
   step_data_128 = line.split(' ', 5)
-  t_128.append(float(step_data_128[0]))
-  x1_128.append(float(step_data_128[1]))
-  y1_128.append(float(step_data_128[2]))
-  x2_128.append(float(step_data_128[3]))
-  y2_128.append(float(step_data_128[4]))
+  t_128.append(np.longdouble(step_data_128[0]))
+  x1_128.append(np.longdouble(step_data_128[1]))
+  y1_128.append(np.longdouble(step_data_128[2]))
+  x2_128.append(np.longdouble(step_data_128[3]))
+  y2_128.append(np.float64(step_data_128[4]))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))

7ee928c4c1f06b4f7fc389ceee1a43cfc094ce72
Wild <wildrl@l25006.cs.jmu.edu>
2018-12-09 16:27:59 -0500

    fixed energy calculation

--- dpend_out.c
+++ dpend_out.c
@@ -50,19 +50,21 @@ void output_cartesian(FILE *file, int nbits, mpfr_t* t, mpfr_t* th1, mpfr_t* w1,
                *t, x1, y1, x2, y2); 
 }
 
-void output_energy(FILE *file, int nbits, mpfr_t* t, mpfr_t* th1, mpfr_t* w1, mpfr_t* th2, mpfr_t* w2, double L1, double L2, double G){
+void output_energy(FILE *file, int nbits, mpfr_t* t, mpfr_t* th1, mpfr_t* w1, mpfr_t* th2, mpfr_t* w2, double L1, double L2, double M1, double M2, double G){
   
   mpfr_t p_energy, k_energy, t_energy, temp, temp2, cos_th1, cos_th2, del, cos_del;
   mpfr_inits2(nbits, p_energy, k_energy, t_energy, temp, temp2, cos_th1, cos_th2, del, cos_del, NULL);
   double half = 0.5;
   double neg = -1.0;
-
+  double mass_sum = M1+M2;
   // PE
   mpfr_cos(cos_th1, *th1, MPFR_RNDN);     
   mpfr_cos(cos_th2, *th2, MPFR_RNDN);
 
   mpfr_mul_d(p_energy, cos_th1, L1, MPFR_RNDN); 
+  mpfr_mul_d(p_energy, p_energy, mass_sum, MPFR_RNDN);
   mpfr_mul_d(temp, cos_th2, L2, MPFR_RNDN);
+  mpfr_mul_d(temp, temp, M2, MPFR_RNDN);
   mpfr_add(p_energy, p_energy, temp, MPFR_RNDN);
   mpfr_mul_d(p_energy, p_energy, -G, MPFR_RNDN);
 
@@ -70,10 +72,12 @@ void output_energy(FILE *file, int nbits, mpfr_t* t, mpfr_t* th1, mpfr_t* w1, mp
   mpfr_mul(temp, *w1, *w1, MPFR_RNDN);
   mpfr_mul_d(temp, temp, L1, MPFR_RNDN);
   mpfr_mul_d(temp, temp, L1, MPFR_RNDN);
+  mpfr_mul_d(temp, temp, M1, MPFR_RNDN);
 
   mpfr_mul(temp2, *w2, *w2, MPFR_RNDN);
   mpfr_mul_d(temp2, temp2, L2, MPFR_RNDN);
   mpfr_mul_d(temp2, temp2, L2, MPFR_RNDN);
+  mpfr_mul_d(temp2, temp2, M2, MPFR_RNDN);
 
   mpfr_add(temp, temp, temp2, MPFR_RNDN);
   mpfr_mul_d(temp, temp, half, MPFR_RNDN);
@@ -81,6 +85,7 @@ void output_energy(FILE *file, int nbits, mpfr_t* t, mpfr_t* th1, mpfr_t* w1, mp
   mpfr_mul(temp2, *w1, *w2, MPFR_RNDN);
   mpfr_mul_d(temp2, temp2, L1, MPFR_RNDN);
   mpfr_mul_d(temp2, temp2, L2, MPFR_RNDN);
+  mpfr_mul_d(temp2, temp2, M2, MPFR_RNDN);
   mpfr_sub(del, *th1, *th2, MPFR_RNDN);
   mpfr_cos(cos_del, del, MPFR_RNDN); 
   mpfr_mul(temp2, temp2, cos_del, MPFR_RNDN);
